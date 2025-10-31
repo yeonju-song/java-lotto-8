c7db433dcb4534ea3682e7dd2f37df397a4a2a5e
yeonju-song <thdduswn2007@naver.com>
2025-10-31 14:58:17 +0900

    Lotto.java 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,15 +1,19 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         int money = inputMoney();
+        int count = money / 1000; //주어진금액으로 살 수 있는 로또 장수 계산
+
 
     }
 
@@ -18,23 +22,28 @@ public class Application {
         System.out.println("구입금액을 입력해 주세요.");
         int money = Integer.parseInt(Console.readLine());
         if (money % 1000 != 0) {
-            throw new IllegalArgumentException("구입 금액은 1,000원 단위여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
         }
         return money;
     }
-    //주어진금액으로 살 수 있는 로또 장수 계산
 
-    //로또번호 만들어서 반환
-    private static List<Integer> printLotto(List<Integer> numbers) {
-        int[] randomNumbers = new int[6];
-        for (int i = 0; i < randomNumbers.length(); i++) {
-            Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    //각 로또의 번호를 랜덤으로 생성
+    private static List<Lotto> makeLottoNumbers(int count) {
+        List<Lotto> lottotickets = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6); //단순히 랜덤으로 뽑음
+            lottotickets.add(new Lotto(numbers)); //모든 로또티켓의 숫자들의 정렬, 검증, 출력을 Lotto.java에서 담당
         }
-        Arrays.sort(randomNumbers);
-        System.out.println(Arrays.toString(randomNumbers));
+        return  lottotickets;
     }
 
-    //여러장의 로또 생성 해서 출력
+    //로또 장수와 번호를 출력
+    private static void printLottoTickets(List<Lotto> lottotickets) {
+        System.out.println(lottotickets.size() + "개를 구매했습니다.");
+        for (Lotto lottoticket : lottotickets) {
+            System.out.println(lottoticket.getNumbers());
+        }
+    }
 
     //당첨번호 입력
     private static List<Integer> inputWinningNumbers() {
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,45 @@
 package lotto;
+//로또 한장이 가지는 번호, 검증, 출력 기능을 여기서 책임
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    // 로또 번호 저장
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validate(numbers); //유효성 검사
+        Collections.sort(numbers); //오름차순으로 정리
         this.numbers = numbers;
     }
 
+    // 로또 번호 검증 (번호 6개 확인, 번호 중복 불가, 번호는 1부터 45 사이의 숫자)
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        long checkDuplicate = numbers.stream().distinct().count();
+        if (checkDuplicate != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 중복됐습니다.");
+        }
+
+        boolean checkRange = numbers.stream().anyMatch(number -> number <1 || number > 45);
+        if (checkRange) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호가 1부터 45 사이의 숫자가 아닙니다.");
+        }
+        //오름차순 정리인지 검증을 하지 않는 이유는 오름차순 정렬을 생성자에서 직접 수행하기때문이다. 실패할일이 없음
     }
 
-    // TODO: 추가 기능 구현
+    // 안전하게 번호만 읽을수 있도록
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    //출력 형식을 일관적으로
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
